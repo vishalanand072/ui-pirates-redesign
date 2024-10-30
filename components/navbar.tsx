134a739755cdd92437d78d31b4b63a4916396a9d
DanishAnsari5522 <ddanish1928@gmail.com>
2024-10-30 18:39:53 +0530

    customize header

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,138 +8,114 @@ import {
   NavbarMenuItem,
 } from "@nextui-org/navbar";
 import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
 import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
 import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
 
 import { siteConfig } from "@/config/site";
-import { ThemeSwitch } from "@/components/theme-switch";
-import {
-  TwitterIcon,
-  GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
-  SearchIcon,
-  Logo,
-} from "@/components/icons";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   return (
     <NextUINavbar
       maxWidth="xl"
       position="sticky"
-      className="bg-none blur-none top-0 absolute py-2"
+      className="bg-none mx-[22rem] blur-none py-0 w-auto px-0 max-lg:mx-20 max-md:mx-0 max-xl:mx-40
+      
+      border-2 container  flex flex-row items-center rounded-2xl sticky top-0 mt-3 max-md:mt-0 max-md:rounded-none max-md:border-0
+     
+      h-[55px]
+      "
+      style={{}}
     >
-      <div className="border-2 container  flex flex-row items-center rounded-2xl px-2 py-0 mx-56 max-lg:mx-12 max-md:mx-0 mt-0 sticky top-0">
-        <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-          <NavbarBrand as="li" className="gap-3 max-w-fit">
-            <NextLink
-              className="flex justify-start items-center gap-1"
-              href="/"
-            >
-              {/* <Logo /> */}
-              <img
-                src="https://res.cloudinary.com/damm9iwho/image/upload/v1729862847/Div_framer-bfl99f_v7cltn.svg"
-                alt=""
-                className="mt-2"
-              />
-              <p className="font-bold text-inherit">UI Pirates</p>
-            </NextLink>
-          </NavbarBrand>
-        </NavbarContent>
+      {/* <div
+        className="border-2 container  flex flex-row items-center rounded-2xl sticky top-0"
+      > */}
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <NextLink
+            className="flex justify-start items-center gap-1 md:-ml-6 max-sm:-ml-3"
+            href="/"
+          >
+            {/* <Logo /> */}
+            <img
+              src="https://res.cloudinary.com/damm9iwho/image/upload/v1729862847/Div_framer-bfl99f_v7cltn.svg"
+              alt=""
+              className="mt-2"
+            />
+            <p className="font-bold text-inherit">UI Pirates</p>
+          </NextLink>
+        </NavbarBrand>
+      </NavbarContent>
 
-        <NavbarContent className="basis-1/5 sm:basis-full" justify="center">
-          <ul className="hidden lg:flex gap-4 justify-start ml-2">
-            {siteConfig.navItems.map((item) => (
-              <NavbarItem key={item.href}>
-                <NextLink
-                  className={clsx(
-                    linkStyles({ color: "foreground" }),
-                    "data-[active=true]:text-primary data-[active=true]:font-medium"
-                  )}
-                  color="foreground"
-                  href={item.href}
-                >
-                  {item.label}
-                </NextLink>
-              </NavbarItem>
-            ))}
-          </ul>
-        </NavbarContent>
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="center">
+        <ul className="hidden lg:flex gap-4 justify-start ml-2">
+          {siteConfig.navItems.map((item) => (
+            <NavbarItem key={item.href}>
+              <NextLink
+                className={clsx(
+                  linkStyles({ color: "foreground" }),
+                  "data-[active=true]:text-primary data-[active=true]:font-medium"
+                )}
+                color="foreground"
+                href={item.href}
+              >
+                {item.label}
+              </NextLink>
+            </NavbarItem>
+          ))}
+        </ul>
+      </NavbarContent>
 
-        <NavbarContent
-          className="hidden sm:flex basis-1/5 sm:basis-full"
-          justify="end"
-        >
-          <NavbarItem className="hidden md:flex">
-            <Button
-              isExternal
-              as={Link}
-              className="text-sm font-normal text-white bg-black pt-0 dark:bg-white dark:text-black"
-              // startContent={<HeartFilledIcon className="text-danger" />}
-              variant="solid"
-              style={{ paddingTop: 0 }}
-            >
-              Have an Idea?
-            </Button>
-          </NavbarItem>
-        </NavbarContent>
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        justify="end"
+      >
+        <NavbarItem className="hidden md:flex">
+          <Button
+            isExternal
+            as={Link}
+            className="text-sm font-normal text-white bg-black pt-0 dark:bg-white dark:text-black -mr-4"
+            // startContent={<HeartFilledIcon className="text-danger" />}
+            variant="solid"
+            style={{ paddingTop: 0 }}
+          >
+            Have an Idea?
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
 
-        <NavbarContent className="sm:hidden basis-1" justify="end">
-          {/* <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+      <NavbarContent className="sm:hidden basis-1" justify="end">
+        {/* <Link isExternal aria-label="Github" href={siteConfig.links.github}>
             <GithubIcon className="text-default-500" />
           </Link>
           <ThemeSwitch /> */}
-          <NavbarMenuToggle />
-        </NavbarContent>
+        <NavbarMenuToggle />
+      </NavbarContent>
 
-        <NavbarMenu>
-          <div className="mx-4 mt-2 flex flex-col gap-2">
-            {siteConfig.navMenuItems.map((item, index) => (
-              <NavbarMenuItem key={`${item}-${index}`}>
-                <Link
-                  // color={
-                  //   index === 2
-                  //     ? "primary"
-                  //     : index === siteConfig.navMenuItems.length - 1
-                  //     ? "danger"
-                  //     : "foreground"
-                  // }
-                  color="foreground"
-                  href="#"
-                  size="lg"
-                >
-                  {item.label}
-                </Link>
-              </NavbarMenuItem>
-            ))}
-          </div>
-        </NavbarMenu>
-      </div>
+      <NavbarMenu>
+        <div className="mx-4 mt-2 flex flex-col gap-2">
+          {siteConfig.navMenuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link
+                // color={
+                //   index === 2
+                //     ? "primary"
+                //     : index === siteConfig.navMenuItems.length - 1
+                //     ? "danger"
+                //     : "foreground"
+                // }
+                color="foreground"
+                href="#"
+                size="lg"
+              >
+                {item.label}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </div>
+      </NavbarMenu>
+      {/* </div> */}
     </NextUINavbar>
   );
 };
