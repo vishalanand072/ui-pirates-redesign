c1924972fde79f0e35d232603afe8f171d066f15
DanishAnsari5522 <ddanish1928@gmail.com>
2024-10-30 19:01:10 +0530

    add svg image on client text

--- screens/landing/testimonials/index.tsx
+++ screens/landing/testimonials/index.tsx
@@ -12,7 +12,17 @@ function LandingTestimonials() {
           />
         </div>
         <p className="text-center text-5xl max-lg:text-4xl max-md:text-2xl font-semibold">
-          Working with our clients partners
+          Working with our{" "}
+          <span className="relative inline-block ">
+            <span className="z-10">clients</span>
+
+            <img
+              src="https://res.cloudinary.com/damm9iwho/image/upload/v1730284462/Vector_client_jck63k.svg"
+              alt=""
+              className="absolute inset-0 w-[125px] h-full top-[0.4rem] left-2  z-0 max-lg:w-[70px]"
+            />
+          </span>{" "}
+          partners
         </p>
         <div className="flex flex-row items-center justify-center mb-8 mt-5 px-60 max-lg:px-32 max-md:px-8">
           <p className="md:w-1/2 text-center content-center px-16  max-lg:px-8 max-md:px-0">
