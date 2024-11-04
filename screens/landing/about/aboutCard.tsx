8b7a22052a769d3902329f63e061d4c5e539d021
DanishAnsari5522 <ddanish1928@gmail.com>
2024-11-04 19:14:50 +0530

    remove the space in about card top

--- screens/landing/about/aboutCard.tsx
+++ screens/landing/about/aboutCard.tsx
@@ -111,7 +111,7 @@ const AboutCardAnimation = () => {
   };
 
   return (
-    <div className="container mx-auto px-4 py-20 max-md:py-20">
+    <div className="container mx-auto px-4 pb-20 max-md:pb-20">
       <div className="grid grid-cols-2 gap-6 lg:mt-60 max-lg:grid-cols-1">
         {data.map((item, index) => (
           <div
